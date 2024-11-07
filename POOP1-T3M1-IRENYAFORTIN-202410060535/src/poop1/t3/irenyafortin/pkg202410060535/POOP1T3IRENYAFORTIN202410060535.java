d9c1420e5a5f8d25ea56fd573f969f427ad17200
IrenyaFortin <irenya.fortin@gmail.com>
2024-11-06 18:54:33 -0600

    Subida M3

---
+++ POOP1-T3M1-IRENYAFORTIN-202410060535/src/poop1/t3/irenyafortin/pkg202410060535/POOP1T3IRENYAFORTIN202410060535.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1.t3.irenyafortin.pkg202410060535;
+
+/**
+ *
+ * @author IRENYA FORTIN
+ */
+public class POOP1T3IRENYAFORTIN202410060535 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int M=6 ;
+        int T=1;
+        int K=-10;
+        
+        if (M > T){
+            System.out.println(M+" > "+T);
+            System.out.println("Verdadero");
+        }else{
+            System.out.println(M+" > "+T);
+            System.out.println("Falso");
+        }
+        if (T / K == -5){
+             System.out.println(T+" / "+K+ "= -5");
+             System.out.println("Verdadero");
+        }else{
+            System.out.println(T+" / "+K+ "= -5");
+             System.out.println("FALSO");
+        }
+        if (M+T == 7 || M-T == 5){
+             System.out.println(M+" + "+T+ "= 7 || "+M+" - "+T+"  = 5");
+             System.out.println("Verdadero");
+        }else{
+            System.out.println(M+" + "+T+ "= 7 || "+M+" - "+T+"  = 5");
+             System.out.println("FALSO");
+        }
+    }
+    
+}
